b4abd6738db86370c5ce9bbaf616df4fc475b03f
Anton <poliskoanton@gmail.com>
2024-09-24 12:50:13 +0200

    Add files via upload

--- src/main/java/com/shop/myShop/Product.java
+++ src/main/java/com/shop/myShop/Product.java
@@ -1,20 +1,42 @@
 package com.shop.myShop;
 
-public class Product {
-    private String name;
+    public class Product {
+    private final String name;
+    private final String description;
+    private final int price;
+    private final int discount;
 
-    // Конструкторы
-    public Product(String name) {
+
+    public Product(String name,int price ,String description) {
         this.name = name;
+        this.price = price;
+        this.description = description;
+        this.discount = 0;
+    }
+    public Product(String name, int price, String description, int discount) {
+        this.name = name;
+        this.description = description;
+        if (discount < 0 || discount > 100) {
+            throw new IllegalArgumentException("Знижка повинна бути від 0 до 100");
+        }
+        this.discount = discount;
+        this.price = price * (100 - discount) / 100;
     }
 
-    // Геттеры и сеттеры
+
+
+
     public String getName() {
         return name;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    public int getPrice() {
+        return price;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public int getDiscount() {
+        return discount;
     }
 
 }
